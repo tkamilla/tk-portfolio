645cb514e05d914eeb370be9b169903130a0fb01
tkamilla <thomskii93@gmail.com>
2020-11-29 19:30:48 +0800

    fixed home name and text

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,6 +4,7 @@
     <h1>Contact Me</h1>
     <p>Thank you for stopping by my page! If you have any questions or would like to collaborate, don't hesitate to leave a message.</p>
     <div><a href="mailto:thomskii93@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a><span>thomskii93@gmail.com</span></div>
+    <div><a href="https://github.com/tkamilla" target="_blank"><i class="fab fa-github"></i></a><span>github.com/tkamilla</span></div>
     <div><a href="https://www.linkedin.com/in/thomi-kamilla-65226a5b/" target="_blank"><i class="fab fa-linkedin-in"></i></a><span>Thomi Kamilla</span></div>
     <div><a href="https://www.instagram.com/tovetherlife/" target="_blank"><i class="fab fa-instagram"></i></a><span>@tovetherlife</span></div>
     <div><a href="https://www.youtube.com/channel/UCioq2H9ND74G8qnrTCQMYgw" target="_blank"><i class="fab fa-youtube"></i></a><span>@Tovether</span></div>
@@ -48,7 +49,7 @@ section {
 
   p {
     width: 52%;
-    margin: 4rem auto 4rem;
+    margin: 3rem auto 3rem;
     color: #5b4422;
     text-align: center;
   }
@@ -80,7 +81,7 @@ section {
     font-size: 1.4rem;
     p {
       width: 100%;
-      margin: 2.5rem auto;
+      margin: 2.2rem auto;
       line-height: 1.8;
     }
   }
@@ -90,7 +91,7 @@ section {
     font-size: 1.6rem;
     p {
       width: 80%;
-      margin: 4rem auto 5rem;
+      margin: 4rem auto 4rem;
       line-height: 1.8;
     }
 
